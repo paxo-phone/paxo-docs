bac9fa51399f3bab2ebd00ed2eaad22bdf550481
icimax <supermailmax@proton.me>
2025-08-28 14:55:05 +0200

    configurate the doc site

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,21 +9,20 @@ const config: Config = {
   tagline: 'For a better world',
   favicon: 'img/favicon.ico',
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://docs.paxo.fr',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'PAXO', // Usually your GitHub org/user name.
+  projectName: 'paxo-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -57,14 +56,17 @@ const config: Config = {
         },
         blog: {
           showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
+          postsPerPage: 10,
+          routeBasePath: 'blog',
+          include: ['**/*.{md,mdx}'],
+          
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
+
+          blogSidebarCount: 'ALL',
+          blogSidebarTitle: 'Tous les articles',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -75,21 +77,13 @@ const config: Config = {
   /*
   plugins: [
     [
-      '@docusaurus/plugin-client-redirects',
-      {
-        redirects: [
-          {
-            from: '/',
-            to: '/en/',
-          },
-        ],
-      },
+      
     ],
   ],*/
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    //image: 'img/docusaurus-social-card.jpg',
     navbar: {
       logo: {
         alt: 'Paxo Logo',
@@ -103,11 +97,11 @@ const config: Config = {
           position: 'left',
           label: 'Docs',
         },
-        {
-          to: '/blog', 
+        /*{
+          to: '/blog',
           label: 'Blog', 
           position: 'left'
-        },
+        },*/
 
         // -- Éléments de droite --
         {
@@ -154,10 +148,14 @@ const config: Config = {
         {
           title: 'More',
           items: [
-            {
+            /*{
               label: 'Blog',
               to: '/blog',
             },
+            {
+              label: 'Substack',
+              to: '/substack',
+            },*/
             {
               label: 'GitHub',
               href: 'https://github.com/paxo-phone/PaxOS-9',
