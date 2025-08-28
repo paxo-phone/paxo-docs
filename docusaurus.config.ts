2c464717266b54e517dc7b3f73559a3e7917289b
Maxime Houwenaghel <141626084+icimax@users.noreply.github.com>
2025-08-28 15:28:16 +0200

    Update site URL to include HTTPS

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'docs.paxo.fr',
+  url: 'https://docs.paxo.fr',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
